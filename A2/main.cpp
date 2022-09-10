d1612721a054240dcc52524b088c434f7b80dbff
ameena <ameenaelagha@gmail.com>
2022-09-10 20:09:15 +0200

    Adding comments

--- A2/main.cpp
+++ A2/main.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Question 1
 int findMaxElement(const int arr[],int size){
     int max = arr[0];
     
@@ -13,6 +14,7 @@ int findMaxElement(const int arr[],int size){
     return max;
 }
 
+// Question 2
 int computeFactorial(int x){
     if(x < 0){
         return 0;
@@ -24,6 +26,7 @@ int computeFactorial(int x){
         return x*computeFactorial(x-1);
 }
 
+// Question 3
 bool stringPattern(const string& s, const string& p){
     unsigned int pLen = p.length();
     unsigned int sLen = s.length();
@@ -31,7 +34,7 @@ bool stringPattern(const string& s, const string& p){
     if(pLen > sLen)
         return false;
     else{
-        for(int i=0; i<sLen - pLen; i++){
+        for(int i=0; i<= sLen - pLen; i++){
             if(s.substr(i,pLen) == p)
                 return true;
         }
@@ -39,13 +42,15 @@ bool stringPattern(const string& s, const string& p){
     }
 }
 
+// Utility function for string sorting algorithm
 void swap(string *compare, string *compareTo)
 {
     string temp = *compare;
     *compare = *compareTo;
     *compareTo = temp;
 }
- 
+
+// String sort with selection sort
 void selectionSort(string arr[], int n)
 {
     int i, j, min_idx;
@@ -62,6 +67,7 @@ void selectionSort(string arr[], int n)
     }
 }
 
+// Utility function for main
 template <typename T> void printArray(T arr[], int size)
 {
     for(int i = 0; i<size; i++)
@@ -72,23 +78,27 @@ template <typename T> void printArray(T arr[], int size)
 
 
 int main() {
-    
+
+    // Question 1
     int arr[5] = {12,2,1,2,34};
     cout<<"Array: ";
     printArray(arr,5);
     cout<<"Maximum element: "<<findMaxElement(arr,5)<<endl<<endl;
-    
+
+    // Question 2
     int x=3;
     cout<<"Factorial of x="<<x<<" is "<<computeFactorial(3)<<"."<<endl<<endl;
-    
-    string s1 = "seventy";
+
+    // Question 3
+    string s1 = "twenty seventy";
     string s2 = "seven";
     
     if(stringPattern(s1,s2))
         cout << "The string \"" << s1 << "\" contains the pattern \"" << s2 << "\"." << endl << endl;
     else
         cout << "The string \"" << s1 << "\" does not contain the pattern \"" << s2 << "\"." << endl << endl;
-    
+
+    // Question 4 (String Sort)
     string s[6] = {"xyz","abc","abc","abcd","xyz","abca"};
     cout <<"String array before sorting: ";
     printArray(s,6);
